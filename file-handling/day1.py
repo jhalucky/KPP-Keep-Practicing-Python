29d94cef1ae2f5bcf6e7f4d21f09abc8c90cf7ef
Lucky Jha <jhalucky61@gmail.com>
2026-07-21 09:58:53 +0530

    Serialization and deserialization

--- file-handling/day1.py
+++ file-handling/day1.py
@@ -19,8 +19,38 @@
 #     f.writelines(big_l)
     
 
-with open("file-handling/ques1.txt", 'r') as f:
-    chunk_size = 100
+# with open("file-handling/ques1.txt", 'r') as f:
+#     chunk_size = 100
     
-    while len(f.read(chunk_size)) > 0:
-        print(f.read(chunk_size),end="***")
+#     while len(f.read(chunk_size)) > 0:
+#         print(f.read(chunk_size),end="***")
+#         f.read(chunk_size)
+
+# to work with binary files we use binary mode:
+
+# with open("file-handling/screenshot.png", 'rb') as f:
+#     with open("file-handling/screenshot_copy.jpg",'wb') as wf:
+#         wf.write(f.read())
+
+
+# Serialization and deserialization
+
+
+import json
+
+# L = [1,2,3,4]
+d = {
+    "name" : "Lucky",
+    "age" : 5,
+    "height" : "5'6",
+    "weight" : "65 kgs"
+}
+
+t = (1,2,3,4,5)
+
+with open("file-handling/pp.json",'w') as f:
+    d = json.dump(d,f)
+
+with open("file-handling/pp.json", 'r') as rf:
+    d = json.load(rf)
+    print(type(d))
