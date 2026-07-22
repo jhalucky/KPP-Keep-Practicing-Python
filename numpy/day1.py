e2379b697503029e7cc307ed41e671b8df832407
Lucky Jha <jhalucky61@gmail.com>
2026-07-22 18:22:36 +0530

    np.arange

--- numpy/day1.py
+++ numpy/day1.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-f = np.array([[[10,20],[1,2]],[[3,4],[40,10]]])
+# f = np.array([[[10,20],[1,2]],[[3,4],[40,0]]],dtype=complex)
+# print(f)
+# print(type(f))
+
+f = np.arange(1,10,3)
 print(f)
 print(type(f))
