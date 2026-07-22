17a33f6076a7e186aa5fb5fff3e5e2cf01d2965b
Lucky Jha <jhalucky61@gmail.com>
2026-07-22 18:31:22 +0530

    .ones and .zeros()

--- numpy/day1.py
+++ numpy/day1.py
@@ -4,6 +4,14 @@ import numpy as np
 # print(f)
 # print(type(f))
 
-f = np.arange(1,13).reshape(4,3)
-print(f)
-print(type(f))
+# f = np.arange(1,13).reshape(4,3)
+# print(f)
+# print(type(f))
+
+# we use them np.ones() and np.zeros( when we need to create or initilaze an array without passing any values
+
+# f = np.ones((9,10))
+# print(f)
+
+# g = np.zeros((2,3))
+# print(g)
