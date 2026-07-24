f65f058c01072b136a1ba9fc92f97a8bdadd7d14
Lucky Jha <jhalucky61@gmail.com>
2026-07-24 14:44:39 +0530

    boolean and fancy indexing

---
+++ numpy/day2.py
@@ -0,0 +1,40 @@
+# list
+# a = [i for i in range(10000000)]
+# b = [i for i in range(10000000,20000000)]
+# c = []
+
+# import time
+
+# start = time.time()
+# for i in range(len(a)):
+#     c.append(a[i]+b[i])
+# print(time.time() - start)
+
+# # numpy array
+
+import numpy as np
+
+# a1 = np.arange(10000000)
+# a2 = np.arange(10000000,20000000)
+# start = time.time()
+# c = a1 + a2
+# print(time.time() - start)
+
+# # memory
+
+# import sys
+# print(sys.getsizeof(a))
+# print(sys.getsizeof(a1))
+
+
+# Advance Indexing
+
+# a1 = np.arange(16).reshape(4,4)
+# print(a1)
+
+# print(a1[:,[0,1,3]])
+
+# Boolean indexing
+
+a = np.random.randint(1,100,36).reshape(4,9)
+print(a)
