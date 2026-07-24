8fb633c327023c07c3117400fb8bd21b949f3e42
Lucky Jha <jhalucky61@gmail.com>
2026-07-24 19:56:06 +0530

    Mat with numpieee

--- numpy/day2.py
+++ numpy/day2.py
@@ -87,8 +87,8 @@ import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
-x = np.linspace(-10,10,100)
-y = x
+x = np.linspace(10,-10,100)
+y = 1/(1+np.exp(-x))
 
 plt.plot(x,y)
 plt.show()
