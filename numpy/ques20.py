17ae1d40871bfe0e5bb4858df36260afbd26eadd
Lucky Jha <jhalucky61@gmail.com>
2026-07-25 21:15:12 +0530

    numpy chores

---
+++ numpy/ques20.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a1 = np.array([10,20,30,40,50,100,200,300,400,500])
+
+print(f"Mean: {np.mean(a1)}")
+print(f"Median: {np.median(a1)}")
+print(f"Standard Deviation: {np.std(a1)}")
