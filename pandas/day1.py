44ca7c37076e1f111a651abcdd98775b4a3706da
Lucky Jha <jhalucky61@gmail.com>
2026-08-02 18:45:54 +0530

    feat: custom index

--- pandas/day1.py
+++ pandas/day1.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+# str or object dtype
+# country = ['USA', 'Canada', 'London', 'Kingston', 'Germany']
 
-country = ['USA', 'Canada', 'London', 'Kingston', 'Germany']
+# print(pd.Series(country))
 
-print(pd.Series(country))
+# integer
+# run = [24,56,89,100]
 
+# print(pd.Series(run))
 
-run = [24,56,89,100]
+#custom Index
 
-print(pd.Series(run))
+marks = [100, 67, 67, 89, 88]
+subjects = ['English', 'Physics', 'Chemistry', 'Maths', 'Computer Science']
+
+print(pd.Series(marks, index=subjects))
